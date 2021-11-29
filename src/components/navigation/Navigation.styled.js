57f53a535d7fc0f4ae1130fb6f25d9ef4bcc2012
Dasha Kirienko <dkirienko25@gmail.com>
2021-11-29 14:38:59 +0300

    add box-sizing, aling movie list

--- src/components/navigation/Navigation.styled.js
+++ src/components/navigation/Navigation.styled.js
@@ -2,6 +2,7 @@ import styled from '@emotion/styled';
 import { Link, NavLink } from 'react-router-dom';
 
 export const StyledLink = styled(Link)`
+  max-width: 324px;
   display: flex;
   flex-direction: column;
   text-decoration: none;
